717ba5c85c7d991681138c6270e5c71ecaaef15c
Denis Frenademetz <denisfrenademetz97@gmail.com>
2019-03-28 21:53:50 +0100

    feat(Semaphore): initial implementation

---
+++ README.md
@@ -0,0 +1,159 @@
+[![Build Status][build-status-image]][build-status-url]
+[![Coverage Status][coverage-image]][coverage-url]
+[![MIT License][license-image]][license-url]
+
+# Async Semaphore
+
+A counting semaphore based on Java's Concurrent Semaphore.
+
+
+[![https://nodei.co/npm/harperdb-connect.png?downloads=true&downloadRank=true&stars=true](https://nodei.co/npm/harperdb-connect.png?downloads=true&downloadRank=true&stars=true)](https://www.npmjs.com/package/harperdb-connect)
+
+## Install
+
+Install the module via npm:
+
+    npm install ts-async-semaphore
+
+## Quick Example
+
+```typescript
+// fairness false
+
+import {Semaphore} from 'ts-async-semaphore';
+const semaphore = new Semaphore(0);
+
+semaphore.acquire(2).then(() => {
+  console.log('Hello');
+});
+
+semaphore.acquire().then(() => {
+  console.log('World');
+
+  semaphore.release(2);
+});
+
+setTimeout(function() {
+  semaphore.release();
+}, 2000);
+
+```
+
+```typescript
+// fairness true
+
+import {Semaphore} from 'ts-async-semaphore';
+const semaphore = new Semaphore(0, true);
+
+semaphore.acquire(2).then(() => {
+  console.log('Hello');
+});
+
+semaphore.acquire().then(() => {
+    console.log('World');
+})
+
+semaphore.release(1);
+
+setTimeout(function() {
+  semaphore.release(2);
+}, 2000);
+
+```
+
+```typescript
+import {Semaphore} from 'ts-async-semaphore';
+const semaphore = new Semaphore(0, true);
+
+semaphore.tryAcquire(1 , 50).then(val => {
+    console.log(`Acquire ${val ? 'success' : 'fail'}`);
+})
+
+```
+
+## Documentation
+
+#### Constructor: Semaphore([`permits`], [`fairness`])
+
+`permits: Integer` Initial available permits of semaphore. `Default: 0`.
+
+`fairness: Boolean` Fairness of semaphore. If set to `true`, a FIFO rules applied, else, look on each acquirers permit value.
+
+---
+
+#### #availablePermits()
+
+Returns the current number of permits available in this semaphore.
+
+Returns: `number`
+
+---
+
+#### #acquire([`permits`])
+
+Acquires the given number of permits from this semaphore.
+
+`permits: number` The number of permits to acquire. `Default: 0`.
+
+Returns: `Promise<void>`
+---
+
+#### #getQueuedAcquirers()
+
+Returns an estimate of the number of acquirers waiting to acquire.
+
+Returns: `Function[]`
+
+---
+
+#### #getQueueLength()
+
+Returns an estimate of the number of acquirers waiting to acquire.
+
+Returns: `number`
+
+---
+
+#### #release([`permits`])
+
+Releases the given number of permits, returning them to the semaphore.
+
+`permits: number` The number of permits to release. `Default: 0`.
+
+---
+
+#### #drainPermits()
+
+Acquires and returns all permits that are immediately available.
+
+Returns: `number`
+
+---
+
+#### #reducePermits(`permits`)
+
+Shrinks the number of available permits by the indicated reduction.
+
+`permits: number` The number of permits to remove.
+
+---
+
+#### #tryAcquire([`permits`], [`timeoutMs`])
+
+Acquires the given number of permits from this semaphore.
+
+`permits: number` The number of permits to acquire. `Default: 0`.
+`timeoutMs: number` The timeout after which the acquire will fail
+
+Returns: `Boolean` Promise `true` if success, `false` false if fail
+
+[license-image]: http://img.shields.io/badge/license-MIT-blue.svg?style=flat
+[license-url]: LICENSE
+
+[build-status-image]: https://travis-ci.org/Ethan-Arrowood/ts-async-semaphore.svg?branch=master
+[build-status-url]: https://travis-ci.org/Ethan-Arrowood/ts-async-semaphore
+[coverage-image]: https://coveralls.io/repos/github/Ethan-Arrowood/ts-async-semaphore/badge.svg?branch=master
+[coverage-url]: https://coveralls.io/github/Ethan-Arrowood/ts-async-semaphore?branch=master
+[npm-url]: https://npmjs.org/package/ts-async-semaphore
+[npm-version-image]: http://img.shields.io/npm/v/ts-async-semaphore.svg?style=flat
+[npm-downloads-image]: http://img.shields.io/npm/dm/ts-async-semaphore.svg?style=flat
---
+++ src/semaphore.spec.ts
@@ -0,0 +1,157 @@
+import {Semaphore} from "./semaphore";
+
+
+const awaitTime = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+jest.setTimeout(20);
+
+describe('Semaphore', () => {
+
+    test('new Semaphore should create non-fair semaphore', () => {
+        const semaphore = new Semaphore();
+        expect(semaphore.isFair()).toBeFalsy();
+    });
+
+    test('expect acquire() to get 1 permit', async () => {
+        const semaphore = new Semaphore(1);
+
+        expect(semaphore.availablePermits()).toEqual(1);
+
+        await semaphore.acquire();
+
+        expect(semaphore.availablePermits()).toEqual(0);
+    });
+
+    test('expect acquire() should wait till permits are available', async done => {
+        const semaphore = new Semaphore(0);
+        let test = jest.fn();
+
+        expect(await semaphore.tryAcquire()).toBeFalsy();
+
+        semaphore.acquire().then(() => {
+            expect(test).toBeCalled();
+            done()
+        });
+
+        expect(semaphore.hasQueuedAcquirers()).toBeTruthy();
+
+        await awaitTime(10);
+        test();
+        semaphore.release(1);
+    });
+
+    test('expect reducePermit() to reduce permits', async () => {
+        const semaphore = new Semaphore(1);
+
+        expect(await semaphore.tryAcquire()).toBeTruthy();
+
+        semaphore.reducePermits(1);
+
+        expect(await semaphore.tryAcquire()).toBeFalsy();
+    });
+
+    test('expect drainPermits() to reduce permits to 0', async () => {
+        const semaphore = new Semaphore(5);
+
+        expect(await semaphore.tryAcquire(5)).toBeTruthy();
+
+        semaphore.drainPermits();
+
+        expect(semaphore.availablePermits()).toEqual(0);
+    });
+
+    test('expect _checkSemaphore() to be called only after action triggered', async () => {
+        const semaphore = new Semaphore(0);
+
+        const checkSemaphore = jest.spyOn(semaphore as any, '_checkSemaphore');
+
+        await awaitTime(10);
+
+        expect(checkSemaphore).toHaveBeenCalledTimes(0);
+
+        semaphore.release();
+
+        expect(checkSemaphore).toHaveBeenCalledTimes(1);
+    });
+
+    test('expect getQueueLength() to return correct values', async () => {
+        const semaphore = new Semaphore(0);
+
+        semaphore.acquire(2);
+        semaphore.acquire();
+
+        expect(semaphore.getQueueLength()).toEqual(2);
+
+        semaphore.release();
+
+        expect(semaphore.getQueueLength()).toEqual(1);
+
+    });
+
+    test('expect getQueueLength() in fair mode to return correct values', async () => {
+        const semaphore = new Semaphore(0, true);
+
+        semaphore.acquire(2);
+        semaphore.acquire();
+
+        expect(semaphore.getQueueLength()).toEqual(2);
+
+        semaphore.release(1);
+
+        expect(semaphore.getQueueLength()).toEqual(2);
+
+        semaphore.release();
+
+        expect(semaphore.getQueueLength()).toEqual(1);
+    });
+
+    test('expect getQueueAcquirers() in fair mode to return correct values', async () => {
+        const semaphore = new Semaphore(0, true);
+
+        semaphore.acquire(2);
+        semaphore.acquire();
+
+        expect(semaphore.getQueuedAcquirers()).toHaveLength(2);
+
+        semaphore.release();
+
+        expect(semaphore.getQueuedAcquirers()).toHaveLength(2);
+
+        semaphore.release();
+
+        expect(semaphore.getQueuedAcquirers()).toHaveLength(1);
+    });
+
+    test('expect tryAcquire() with timeout to return true', async () => {
+        const semaphore = new Semaphore(0, true);
+
+        await Promise.all([
+            expect(semaphore.tryAcquire(2, 10)).resolves.toBeTruthy(),
+            awaitTime(5).then(() => semaphore.release(2))
+        ]);
+    });
+
+    test('expect tryAcquire() with timeout to return false after timed out', async () => {
+        const semaphore = new Semaphore(0, true);
+
+        expect(await semaphore.tryAcquire(2, 10)).toBeFalsy();
+    });
+
+    test('expect tryAcquire() not to fail if acquirer is deleted from the array and timeout is called', async () => {
+        const semaphore = new Semaphore(0, true);
+
+        await Promise.all([
+            expect(semaphore.tryAcquire(2, 10)).resolves.toBeFalsy(),
+            (semaphore as any).acquirers.splice(0, 1)
+        ]);
+    });
+
+    test('expect calls with wrong parameter to fail if is not a valid value', async () => {
+        const semaphore = new Semaphore();
+
+        expect(() => semaphore.reducePermits(-1)).toThrowError();
+        expect(() => semaphore.release(-1)).toThrowError();
+        await expect(semaphore.tryAcquire(-1)).rejects.toThrowError();
+        await expect(semaphore.acquire(-1)).rejects.toThrowError();
+    });
+});
